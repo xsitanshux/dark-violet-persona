6d12d8a5dd32bd413af2ec648bb95e0c23934acd
Batch17-repo <major.batch.17@gmail.com>
2025-07-06 01:11:59 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,7 +28,7 @@ function Hero() {
           typingIndex.current++;
         } else {
           clearInterval(typingInterval);
-          setTimeout(() => setIsTyping(false), 2000);
+          setTimeout(() => setIsTyping(false), 1000);
         }
       }, 100);
 
