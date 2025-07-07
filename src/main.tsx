c371de5d8cf24aa7f66d4dd924cee09ad1f46d94
xsitanshux <sitanshu4work@gmail.com>
2025-07-08 00:04:45 +0530

    completed local edits, ready for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/dark-violet-persona/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
